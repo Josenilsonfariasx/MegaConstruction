756df51e4296f5ab65b331b3e40c39dc34d2a17a
Josenilson Farias <fariaslwork@gmail.com>
2023-07-30 14:55:00 -0300

    feat: adding data mock

---
+++ src/data/category.js
@@ -0,0 +1,6 @@
+export const categories = [
+    { id: 1, name: 'Ferramentas' },
+    { id: 2, name: 'Materiais de Construção' },
+    { id: 3, name: 'Iluminação' },
+    { id: 4, name: 'Pintura' }
+];
---
+++ src/data/products.js
@@ -0,0 +1,29 @@
+export const productsContrution = [
+    // Ferramentas
+    { id: 1, name: 'Martelo de Carpinteiro', category: 'Ferramentas', price: 35.00 },
+    { id: 2, name: 'Trena de Medição', category: 'Ferramentas', price: 18.50 },
+    { id: 3, name: 'Serra Circular', category: 'Ferramentas', price: 120.00 },
+    { id: 4, name: 'Chave de Fenda Conjunto', category: 'Ferramentas', price: 22.00 },
+    { id: 5, name: 'Lixadeira Elétrica', category: 'Ferramentas', price: 75.00 },
+
+    // Materiais de Construção
+    { id: 6, name: 'Tijolos Cerâmicos (unidade)', category: 'Materiais de Construção', price: 1.80 },
+    { id: 7, name: 'Telhas de Cerâmica (unidade)', category: 'Materiais de Construção', price: 5.00 },
+    { id: 8, name: 'Cimento (saco de 50kg)', category: 'Materiais de Construção', price: 25.00 },
+    { id: 9, name: 'Argamassa (saco de 20kg)', category: 'Materiais de Construção', price: 18.00 },
+    { id: 10, name: 'Areia (metro cúbico)', category: 'Materiais de Construção', price: 50.00 },
+
+    // Iluminação
+    { id: 11, name: 'Luminária de Teto', category: 'Iluminação', price: 40.00 },
+    { id: 12, name: 'Lâmpada LED (pacote com 4 unidades)', category: 'Iluminação', price: 12.00 },
+    { id: 13, name: 'Refletor LED 50W', category: 'Iluminação', price: 55.00 },
+    { id: 14, name: 'Spot Embutido (unidade)', category: 'Iluminação', price: 8.50 },
+    { id: 15, name: 'Pendente para Sala', category: 'Iluminação', price: 30.00 },
+
+    // Pintura
+    { id: 16, name: 'Tinta Acrílica (lata de 3,6 litros)', category: 'Pintura', price: 55.00 },
+    { id: 17, name: 'Rolo de Pintura', category: 'Pintura', price: 8.50 },
+    { id: 18, name: 'Pincel para Tinta à Base de Água', category: 'Pintura', price: 6.00 },
+    { id: 19, name: 'Massa Corrida (pote de 1kg)', category: 'Pintura', price: 12.00 },
+    { id: 20, name: 'Tinta Spray Multiuso', category: 'Pintura', price: 10.00 },
+];
